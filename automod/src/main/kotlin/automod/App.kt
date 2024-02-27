03a85b37f281340d77f53a7da82cf1a99cb7e246
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2024-02-27 17:06:53 +0000

    Commit: Pine stuff to app

--- automod/src/main/kotlin/automod/App.kt
+++ automod/src/main/kotlin/automod/App.kt
@@ -1,7 +1,11 @@
 package auto.mod
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import android.util.Log
+import top.canyie.pine.Pine
+import top.canyie.pine.callback.MethodHook
 
 open class App : Application() {
 
@@ -11,5 +15,14 @@ open class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Log.e("AUTOMOD", "It works!")
+        Pine.hook(Activity::class.java.getDeclaredMethod("onCreate", Bundle::class.javaObjectType), object : MethodHook() {
+            override fun beforeCall(callFrame: Pine.CallFrame) {
+                Log.e("AUTOMOD", "Before " + callFrame.thisObject + " onCreate()")
+            }
+
+            override fun afterCall(callFrame: Pine.CallFrame) {
+                Log.e("AUTOMOD", "After " + callFrame.thisObject + " onCreate()")
+            }
+        })
     }
 }
