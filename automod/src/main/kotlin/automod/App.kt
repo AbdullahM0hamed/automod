50823bac39f53571afb53387eb5b93d13ff98398
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2024-03-03 18:26:21 +0000

    Zipalign with uncompressed resources.arsc

--- automod/src/main/kotlin/automod/App.kt
+++ automod/src/main/kotlin/automod/App.kt
@@ -6,9 +6,13 @@ import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
 import android.util.Log
+import android.widget.Toast
 import auto.mod.plugin.PluginManager
 import top.canyie.pine.Pine
 import top.canyie.pine.callback.MethodHook
@@ -18,18 +22,30 @@ open class App : Application() {
         super.onCreate()
         android.util.Log.e("AUTOMOD", "App Test")
         PluginManager.loadPlugins(applicationContext)
-        Pine.hook(Activity::class.java.getDeclaredMethod("dispatchTouchEvent", MotionEvent::class.javaObjectType), object : MethodHook() {
+        android.widget.Toast.makeText(applicationContext, "Auto mod 1", 5).show()
+        android.util.Log.e("AUTOMOD",  "Auto mod 1")
+        /*Pine.hook(Activity::class.java.getDeclaredMethod("dispatchTouchEvent", MotionEvent::class.javaObjectType), object : MethodHook() {
             override fun afterCall(callFrame: Pine.CallFrame) {
-                val event = callFrame.args[0] as MotionEvent
+		val event = callFrame.args[0] as MotionEvent
                 val counter = event.pointerCount
                 val activity = callFrame.thisObject as Activity
-
-                //Will I change this code immediately? Yes. 
-                //But do I have boxes to check? Yes.
+                android.util.Log.e("AUTOMOD", "Counter: " + counter)
+                /*if (counter == 2) {
+                    activity.startActivity(
+                        Intent(
+                            activity,
+                            SettingsActivity::class.java
+                        )
+                    )
+                }*/
+		Log.e("AUTOMOD", event.toString())
                 if (event != null) {
                     val action = event.action and MotionEvent.ACTION_MASK
+		    Log.e("AUTOMOD", "Action: " + event.action.toString())
                     when (action) {
                         MotionEvent.ACTION_POINTER_DOWN -> {
+                            android.util.Log.e("AUTOMOD", "Counter: " + counter)
+>>>>>>> f26fd7f (Zipalign with uncompressed resources.arsc)
                             if (counter == 2) {
                                 activity.startActivity(
                                     Intent(
@@ -43,6 +59,52 @@ open class App : Application() {
                 }
             }
             override fun beforeCall(callFrame: Pine.CallFrame) {}
-        })
+        })*/
+        val handler = Handler(Looper.getMainLooper())
+        Pine.hook(
+            Activity::class.java.getDeclaredMethod(
+                "onKeyDown",
+                Int::class.javaPrimitiveType,
+                KeyEvent::class.javaObjectType
+            ),
+            object : MethodHook() {
+                override fun beforeCall(callFrame: Pine.CallFrame) {}
+                override fun afterCall(callFrame: Pine.CallFrame) {
+                    android.widget.Toast.makeText(applicationContext, "Auto mod 2", 5).show()
+                    android.util.Log.e("AUTOMOD",  "Auto mod 2")
+                    val keyCode = callFrame.args[0] as Int
+                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+                        val activity = callFrame.thisObject as Activity
+                        handler.postDelayed({
+                            activity.startActivity(
+                                Intent(
+                                    activity,
+                                    SettingsActivity::class.java
+                                )
+                            )
+                        }, 1000)
+                    }
+                }
+            }
+        )
+
+        Pine.hook(
+            Activity::class.java.getDeclaredMethod(
+                "onKeyUp",
+                Int::class.javaPrimitiveType,
+                KeyEvent::class.javaObjectType
+            ),
+            object : MethodHook() {
+                override fun beforeCall(callFrame: Pine.CallFrame) {}
+                override fun afterCall(callFrame: Pine.CallFrame) {
+                    android.widget.Toast.makeText(applicationContext, "Auto mod 3", 5).show()
+                    android.util.Log.e("AUTOMOD",  "Auto mod 3")
+                    val keyCode = callFrame.args[0] as Int
+                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+                        handler.removeCallbacksAndMessages(null)
+                    }
+                }
+            }
+        )
     }
 }
