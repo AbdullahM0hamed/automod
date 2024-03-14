6c1ef21ecad0703fefca11afeea8494c5e0c2fb5
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2024-03-14 23:59:16 +0000

    Add repo Edittext: focus handling

--- automod/src/main/kotlin/automod/SettingsActivity.kt
+++ automod/src/main/kotlin/automod/SettingsActivity.kt
@@ -1,10 +1,14 @@
 package auto.mod
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -15,29 +19,48 @@ class SettingsActivity : Activity() {
     override fun onCreate(instance: Bundle?) {
         super.onCreate(instance)
 
-	try {
+        try {
             val parser = assets.openXmlResourceParser("assets/automod.xml")
             val inflater = LayoutInflater.from(this)
             val view = inflater.inflate(parser, LinearLayout(this))
             setContentView(view)
 
             val infoTextView = view.findViewById(2131165186) as TextView
-	    infoTextView.text = "Add additional repos to automod. This should be a URL that ends with \"updater.json\""
+            infoTextView.text = "Add additional repos to automod. This should be a URL that ends with \"updater.json\""
             val color = infoTextView.currentTextColor
             val bg = color xor 0x00ffffff.toInt()
             view.setBackgroundColor(bg)
 
-            //TODO: make it have the right drawable when beinf typed in only 
-            /*val editable = view.findViewById(2131165188) as TextView
-            editable.setColorFilter(color, PorterDuff.Mode.SRC_IN)
-	    val add = getAssetDrawable("done.xml")
-            editable.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                null,
-                null,
-                add,
-                null
-            )*/
+            val add = getAssetDrawable("done.xml")
+            val editable = view.findViewById(2131165188) as TextView
+            editable.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    editable.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        null,
+                        null,
+                        add,
+                        null
+                    )
+                } else {
+                    editable.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                }
+            }
+            editable.setOnTouchListener { edittext, event ->
+                if (event.action == MotionEvent.ACTION_UP && event.rawX >= edittext.right - (edittext as TextView).totalPaddingRight) {
+                    edittext.clearFocus()
+                    val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    manager.hideSoftInputFromWindow(edittext.windowToken, 0)
+                    return@setOnTouchListener true
+                }
 
+                return@setOnTouchListener false
+            }
+            add?.setColorFilter(color, PorterDuff.Mode.SRC_IN)
             val helpIconView = view.findViewById(2131165185) as ImageView
             helpIconView.setColorFilter(color, PorterDuff.Mode.SRC_IN)
             val helpIcon = getAssetDrawable("help.xml")
